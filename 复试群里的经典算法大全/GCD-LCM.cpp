48734d54209589bb3d5238441ef200886c649268
JoyChenCN <354215459@qq.com>
2020-03-27 23:09:31 +0800

    relax 19d

--- 复试群里的经典算法大全/GCD-LCM.cpp
+++ 复试群里的经典算法大全/GCD-LCM.cpp
@@ -1,9 +1,17 @@
-#include <stdio.h> #include <stdlib.h>
-int main(void) { int m, n, r;
-int s;
-printf("输入两数:"); scanf("%d %d", &m, &n); s = m * n;
-while(n != 0) { r = m % n;
-m = n;
-n = r; }
-printf("GCD:%d\n", m); printf("LCM:%d\n", s/m);
-return 0; }
+#include <iostream> 
+using namespace std;
+int main() {
+    int m, n, r;
+    int s;
+    printf("输入两数:");
+    scanf("%d %d", &m, &n);
+    s = m * n;
+    while (n != 0) {
+        r = m % n;
+        m = n;
+        n = r;
+    }
+    printf("GCD:%d\n", m);
+    printf("LCM:%d\n", s / m);
+    return 0;
+}
